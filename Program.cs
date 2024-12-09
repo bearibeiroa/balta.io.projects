6520e1cf25b72256770cac11a93892525986aa9e
bearibeiroa <contato.biaalves@gmail.com>
2024-12-09 17:52:13 -0300

    implementa função subtracao

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.Intrinsics.X86;
+
 Console.WriteLine("Hello, World!");
 
-Soma();
+Subtracao();
 
 static void Soma()
 {
@@ -17,4 +19,21 @@ static void Soma()
     Console.WriteLine($"O resultado da soma é {resultado}");
 
     Console.ReadKey();
+}
+
+static void Subtracao()
+{
+    Console.Clear();
+
+    Console.WriteLine("Digite o primeiro valor:");
+    float valor1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo valor:");
+    float valor2 = float.Parse(Console.ReadLine());
+
+    float resultado = valor1 - valor2;
+    Console.WriteLine($"O resultado da soma é {resultado}");
+
+    Console.ReadKey();
+
 }
