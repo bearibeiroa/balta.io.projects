cad3916c32736ac861fbf31f88901f5868b69f1e
bearibeiroa <contato.biaalves@gmail.com>
2024-12-09 17:46:39 -0300

    implementa função soma

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Soma();
+
+static void Soma()
+{
+    Console.Clear();    
+
+    Console.WriteLine("Digite o primeiro valor:");
+    float valor1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo valor:");
+    float valor2 = float.Parse(Console.ReadLine());
+
+    float resultado = valor1 + valor2;
+    Console.WriteLine($"O resultado da soma é {resultado}");
+
+    Console.ReadKey();
+}
